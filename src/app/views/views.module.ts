b0dcfe65433f63df3614acef804ca144bc6c1e30
dsuper66 <david.bullen@gmail.com>
2020-07-25 13:28:00 +1200

    Initial commit

---
+++ src/app/views/views.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NetworkBuilderViewComponent } from './network-builder-view/network-builder-view.component';
+import { DataEntryViewComponent } from './data-entry-view/data-entry-view.component';
+
+import { AppRoutingModule } from '../app-routing.module';
+
+
+
+@NgModule({
+  declarations: [NetworkBuilderViewComponent, DataEntryViewComponent],
+  imports: [
+    CommonModule, AppRoutingModule
+  ]
+})
+export class ViewsModule { }
