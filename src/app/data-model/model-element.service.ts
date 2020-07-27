8a5c37c492677a6119cb241abaf3afacd294e002
David Bullen <david.bullen@gmail.com>
2020-07-27 21:26:02 +1200

    element type fixes

--- src/app/data-model/model-element.service.ts
+++ src/app/data-model/model-element.service.ts
@@ -9,6 +9,23 @@ export class ModelElementService {
   constructor() { }
 
   private modelElements:ModelElement[]=[];
+  private elementNextIndex = new Map<string, bigint>();
 
-  
+  addModelElement(elementType: string): string {
+    //Get next index for i.d.
+    if (this.elementNextIndex[elementType] == undefined) {
+      this.elementNextIndex[elementType] = 1;
+    }
+    let elementIndex = this.elementNextIndex[elementType];
+    let elementId = elementType + elementIndex;
+    this.elementNextIndex[elementType] = elementIndex + 1;
+
+    this.modelElements.push({
+      elementType: elementType,
+      elementId: elementId,
+      name: elementId
+    });
+    return elementId;
+  }
+
 }
